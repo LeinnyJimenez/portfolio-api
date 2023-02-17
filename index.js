f152c9bee0289c515bf5dfdbee1ee9bad41b8f85
LeinnyJimenez <jimenexleinny@gmail.com>
2023-02-16 22:31:47 -0400

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express')
+const express = require('express');
+const cors = require("cors");
 
 // const courses = require('./courses.js')
 
 const app = express();
 
 app.use(express.json());
+
+
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 const courses = [
     {
